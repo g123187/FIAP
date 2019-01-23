69c629132852a972f1bfa8d58b94c6eb3dd74cf9
g123187 <gilmardequeiroz@hotmail.com>
2019-01-23 18:00:28 -0200

    app do trabalho facudade

---
+++ SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.gsilvque.eazisplit.trabalho
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+    }
+}
